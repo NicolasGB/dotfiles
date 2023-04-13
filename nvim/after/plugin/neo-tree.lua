4762973fc58ad23f8499234a1c895140e2cf9184
gouttefangeas <nicolas@tmds.io>
2023-04-13 21:11:37 +0200

    feat(nvim): improve neotree hidden system

--- nvim/after/plugin/neo-tree.lua
+++ nvim/after/plugin/neo-tree.lua
@@ -1,2 +1,22 @@
 require("neo-tree").setup({
+    filesystem = {
+        filtered_items = {
+            visible = false,
+            hide_dotfiles = true,
+            hide_gitignored = true,
+            always_show = {
+                ".gitignore",
+                ".gitlab-ci.yml",
+                "target",
+                "Cargo.lock",
+                ".env",
+                ".ssh",
+
+                -- Kore
+                ".env.local",
+                "docker-compose.override.yaml"
+
+            },
+        }
+    }
 })
