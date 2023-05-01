ed905e341c44fe46acb230401a8ad7b3c1ef82b7
NicolasGB <nicolas@tmds.io>
2023-05-01 14:46:04 +0200

    feat(nvim): Add illuminate and fsread

---
+++ nvim/lua/plugins/vim-illuminate.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "RRethy/vim-illuminate",
+        event = { "BufReadPost", "BufNewFile" },
+        opts = { delay = 200 },
+        lazy = false,
+        config = function()
+            require("illuminate").configure({})
+        end,
+    },
+}
