a16729504dd7e257204532764aeeb504ab85b6b6
NicolasGB <nicolas@tmds.io>
2023-07-07 14:43:25 +0200

    fix(nvim): change hop bindings

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -12,11 +12,15 @@ return {
         hop.setup({})
 
 
-        vim.keymap.set('', 'f', function()
-            hop.hint_words({ direction = directions.AFTER_CURSOR, current_line_only = false })
-        end, { remap = true })
-        vim.keymap.set('', 'F', function()
-            hop.hint_words({ direction = directions.BEFORE_CURSOR, current_line_only = false })
-        end, { remap = true })
+        vim.keymap.set('', 'f', ":HopWordMW<CR>", { noremap = true, silent = true })
+
+        vim.keymap.set('', 't', ":HopChar1CurrentLine<CR>", { noremap = true, silent = true })
+        --
+        -- vim.keymap.set('', 'f', function()
+        --     hop.hint_words({ direction = directions.AFTER_CURSOR, current_line_only = false })
+        -- end, { remap = true })
+        -- vim.keymap.set('', 'F', function()
+        --     hop.hint_words({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+        -- end, { remap = true })
     end,
 }
