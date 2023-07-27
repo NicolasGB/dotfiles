1b4e458e4f702cae63d3f48f3296490f1e61ddf4
NicolasGB <nicolas@tmds.io>
2023-07-27 09:45:59 +0200

    fix(nvim): remove fixed verison from telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.1',
+        branch = '0.1.x',
         -- or                              , branch = '0.1.1',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
