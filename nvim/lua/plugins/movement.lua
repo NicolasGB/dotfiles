7edd0ac3a4c61d5bfe0a6b2000a295b026a7fa81
NicolasGB <nicolas@tmds.io>
2023-10-25 17:37:13 +0200

    fix(nvim): remove flash for now

--- nvim/lua/plugins/movement.lua
+++ nvim/lua/plugins/movement.lua
@@ -25,27 +25,27 @@ return {
             vim.api.nvim_set_hl(0, 'HopNextKey2', { fg = '#A7C080', bold = true, ctermfg = 198, cterm = { bold = true } })
         end,
     },
-    {
-        "folke/flash.nvim",
-        event  = "VeryLazy",
-        keys   = {
-            -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-            {
-                "S",
-                mode = { "n", "x", "o" },
-                function() require("flash").treesitter() end,
-                desc = "Flash Treesitter"
-            },
-        },
-        config = function()
-            local Flash = require("flash")
-            Flash.setup({})
-
-            vim.cmd('highlight CustomFlash guifg=#A7C080 guibg=NONE ctermfg=red ctermbg=NONE')
-            vim.cmd('highlight CustomFlash2 guifg=#A7C080 guibg=NONE ctermfg=red ctermbg=NONE')
-
-            -- Remote flash
-            vim.keymap.set('o', "r", Flash.remote, { noremap = true, silent = true })
-        end
-    }
+    -- {
+    --     "folke/flash.nvim",
+    --     event  = "VeryLazy",
+    --     keys   = {
+    --         -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    --         {
+    --             "S",
+    --             mode = { "n", "x", "o" },
+    --             function() require("flash").treesitter() end,
+    --             desc = "Flash Treesitter"
+    --         },
+    --     },
+    --     config = function()
+    --         local Flash = require("flash")
+    --         Flash.setup({})
+    --
+    --         vim.cmd('highlight CustomFlash guifg=#A7C080 guibg=NONE ctermfg=red ctermbg=NONE')
+    --         vim.cmd('highlight CustomFlash2 guifg=#A7C080 guibg=NONE ctermfg=red ctermbg=NONE')
+    --
+    --         -- Remote flash
+    --         vim.keymap.set('o', "r", Flash.remote, { noremap = true, silent = true })
+    --     end
+    -- }
 }
