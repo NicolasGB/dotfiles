7dc05c24494b16823682094d741b5c5556f9914e
NicolasGB <nicolas@tmds.io>
2023-11-07 13:49:11 +0100

    fix(wezterm): remap ctrl q

--- .wezterm.lua
+++ .wezterm.lua
@@ -47,8 +47,39 @@ config.max_fps = 144
 
 -- Disable fullscreen keybinding to prevent overlap with code actions
 config.keys = {
-    { key = "Enter", mods = "ALT", action = wezterm.action.DisableDefaultAssignment },
+    { key = "Enter", mods = "ALT",  action = wezterm.action.DisableDefaultAssignment },
+    { key = "q",     mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
 }
+
+-- Enable hyperlinks
+-- Use the defaults as a base
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+-- make task numbers clickable
+-- the first matched regex group is captured in $1.
+table.insert(config.hyperlink_rules, {
+    regex = [[\b[tt](\d+)\b]],
+    format = 'https://example.com/tasks/?t=$1',
+})
+
+-- make username/project paths clickable. this implies paths like the following are for github.
+-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+-- as long as a full url hyperlink regex exists above this it should not match a full url to
+-- github or gitlab / bitbucket (i.e. https://gitlab.com/user/project.git is still a whole clickable url)
+table.insert(config.hyperlink_rules, {
+    regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+    format = 'https://www.github.com/$1/$3',
+})
+
+config.mouse_bindings = {
+    -- Ctrl-click will open the link under the mouse cursor
+    {
+        event = { Up = { streak = 1, button = 'Left' } },
+        action = wezterm.action.OpenLinkAtMouseCursor,
+    },
+}
+
+
 
 -- and finally, return the configuration to wezterm
 return config
