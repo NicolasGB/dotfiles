1222a67d9d0fbb80ffd184013d900666e7f18133
NicolasGB <ngou0210@gmail.com>
2024-07-26 14:38:53 +0200

    fix(nvim): update missing config on mini.ai

--- nvim/lua/plugins/mini-ai.lua
+++ nvim/lua/plugins/mini-ai.lua
@@ -1,3 +1,14 @@
 return {
-  { "echasnovski/mini.ai", version = "*" },
+  {
+    "echasnovski/mini.ai",
+    version = "*",
+    config = function()
+      local spec_treesitter = require("mini.ai").gen_spec.treesitter
+      require("mini.ai").setup {
+        custom_textobjects = {
+          f = spec_treesitter { a = "@function.outer", i = "@function.inner" },
+        },
+      }
+    end,
+  },
 }
