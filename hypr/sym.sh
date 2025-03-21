70cd91727eb8a08fece8824cc787ad91b79cdc8d
NicolasGB <ngou0210@gmail.com>
2025-03-21 09:37:15 +0100

    feat(hypr): Add hyprland ml4w custom dotfiles

---
+++ hypr/sym.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+dotfiles_dir=~/dotfiles/hypr
+config_dir=~/.config/hypr
+
+create_symlink() {
+    source_path="$dotfiles_dir/$1"
+    target_path="$config_dir/$2"
+
+    if [ -e "$target_path" ]; then
+        echo "Symbolic link for $1 already exists."
+    else
+        ln -s "$source_path" "$target_path"
+        echo "Symbolic link for $1 created."
+    fi
+}
+
+remove_symlink() {
+    target_path="$config_dir/$1"
+
+    if [ -L "$target_path" ]; then
+        rm "$target_path"
+        echo "Symbolic link for $1 removed."
+    else
+        echo "No symbolic link found for $1."
+    fi
+}
+
+case "$1" in
+    "create")
+        create_symlink conf/custom.conf conf/custom.conf
+        create_symlink conf/windowrules/custom.conf conf/windowrules/custom.conf
+        create_symlink conf/keybindings/custom.conf conf/keybindings/custom.conf
+        echo "Setup completed!"
+        ;;
+    "remove")
+        remove_symlink conf/custom.conf 
+        remove_symlink conf/windowrules/custom.conf 
+        remove_symlink conf/keybindings/custom.conf 
+        echo "Removal completed!"
+        ;;
+    *)
+        echo "Usage: $0 [create|remove]"
+        exit 1
+        ;;
+esac
+
+
